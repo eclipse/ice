fa95ed68ea107aaef8b114ae612d91930e92e24a
Jay Jay Billings <billingsjj@ornl.gov>
2015-01-06 10:51:58 -0500

    Added skeleton code for AbstractItemBuilderTester.
    Signed-off-by: Jay Jay Billings <billingsjj@ornl.gov>

---
+++ tests/org.eclipse.ice.item.test/src/org/eclipse/ice/item/test/AbstractItemBuilderTester.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2013, 2014 UT-Battelle, LLC.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Initial API and implementation and/or initial documentation - 
+ *   Jay Jay Billings
+ *******************************************************************************/
+package org.eclipse.ice.item.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * This class is responsible for testing the AbstractItemBuilder.
+ * 
+ * @author Jay Jay Billings
+ * 
+ */
+public class AbstractItemBuilderTester {
+
+	/**
+	 * Test method for
+	 * {@link org.eclipse.ice.item.AbstractItemBuilder#getItemName()}.
+	 */
+	@Test
+	public void testGetItemName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link org.eclipse.ice.item.AbstractItemBuilder#build(org.eclipse.core.resources.IProject)}
+	 * .
+	 */
+	@Test
+	public void testBuild() {
+		fail("Not yet implemented");
+	}
+
+}
