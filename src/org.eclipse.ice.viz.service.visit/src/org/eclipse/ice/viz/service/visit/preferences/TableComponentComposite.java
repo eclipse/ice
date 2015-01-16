bfb81136d036d742b8ade303b1936aedc1e9eb59
Jordan Deyton <deytonjh@ornl.gov>
2015-01-16 16:56:44 -0500

    Added an add and remove button to add and remove rows to the TableComponent of connections.
    Signed-off-by: Jordan Deyton <deytonjh@ornl.gov>

--- src/org.eclipse.ice.viz.service.visit/src/org/eclipse/ice/viz/service/visit/preferences/TableComponentComposite.java
+++ src/org.eclipse.ice.viz.service.visit/src/org/eclipse/ice/viz/service/visit/preferences/TableComponentComposite.java
@@ -1,11 +1,20 @@
 package org.eclipse.ice.viz.service.visit.preferences;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.ice.datastructures.form.TableComponent;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.ActionContributionItem;
 import org.eclipse.jface.viewers.ColumnViewerToolTipSupport;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.window.ToolTip;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.ui.PlatformUI;
@@ -25,6 +34,22 @@ public class TableComponentComposite extends Composite {
 	private TableViewer tableViewer;
 
 	/**
+	 * Adds a new, default row to the {@link #tableComponent}.
+	 */
+	private Action addRowAction;
+	/**
+	 * Removes all selected rows from the {@link #tableComponent}.
+	 */
+	private Action removeRowAction;
+
+	// TODO Add an add wizard action.
+	
+	/**
+	 * A list to keep track of selected indices in the {@code TableComponent}.
+	 */
+	private final List<Integer> selectedIndices = new ArrayList<Integer>();
+	
+	/**
 	 * The default constructor.
 	 * 
 	 * @param parent
@@ -36,12 +61,26 @@ public class TableComponentComposite extends Composite {
 	public TableComponentComposite(Composite parent, int style) {
 		super(parent, style);
 
-		// TODO Add add/remove/wizard buttons and change the layout.
-		// For now, use a FillLayout.
-		setLayout(new FillLayout());
+		// Use a 2-column GridLayout. The table should get all excess space and
+		// should be on the left. The action buttons should be on the right.
+		setLayout(new GridLayout(2, false));
 
 		// Create the TableViewer.
 		tableViewer = createTableViewer(this);
+		tableViewer.getTable().setLayoutData(
+				new GridData(SWT.FILL, SWT.FILL, true, true));
+
+		// Initialize the Actions.
+		createActions();
+		// Create a Composite to contain the buttons. Use a vertical fill
+		// layout.
+		Composite buttons = new Composite(this, SWT.NONE);
+		buttons.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, false,
+				false));
+		buttons.setLayout(new FillLayout(SWT.VERTICAL));
+		// Create Buttons from the JFace Actions.
+		new ActionContributionItem(addRowAction).fill(buttons);
+		new ActionContributionItem(removeRowAction).fill(buttons);
 
 		return;
 	}
@@ -67,6 +106,11 @@ public class TableComponentComposite extends Composite {
 		PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
 			@Override
 			public void run() {
+				// Make sure the remove action is disabled since the
+				// TableComponent is new.
+				removeRowAction.setEnabled(false);
+				selectedIndices.clear();
+				// Set the TableViewer's input.
 				tableViewer.setInput(tableComponent);
 			}
 		});
@@ -93,10 +137,10 @@ public class TableComponentComposite extends Composite {
 	 */
 	private TableViewer createTableViewer(Composite parent) {
 		// Create the TableViewer and the underlying Table Control.
-		TableViewer viewer = new TableViewer(this, SWT.BORDER
+		TableViewer viewer = new TableViewer(this, SWT.BORDER | SWT.MULTI
 				| SWT.FULL_SELECTION | SWT.H_SCROLL | SWT.V_SCROLL);
 		// Set some properties for the table.
-		Table table = viewer.getTable();
+		final Table table = viewer.getTable();
 		table.setHeaderVisible(true);
 		table.setLinesVisible(true);
 
@@ -107,7 +151,78 @@ public class TableComponentComposite extends Composite {
 		// Enable tool tips for the Table's cells.
 		ColumnViewerToolTipSupport.enableFor(viewer, ToolTip.NO_RECREATE);
 
+		// Add a selection listener to tell the TableComponent which rows are
+		// selected.
+		ISelectionChangedListener listener = new ISelectionChangedListener() {
+			@Override
+			public void selectionChanged(SelectionChangedEvent event) {
+
+				// To delete rows (later), the TableComponent expects indices.
+				// Thus, we must keep track of selected indicies by updating the
+				// class' list.
+
+				// To select rows, the TableComponent expects an
+				// ArrayList<Integer> containing the row IDs, not indices. Thus,
+				// we must convert the int[] of indices into an
+				// ArrayList<Integer> of IDs to select.
+				int[] selected = table.getSelectionIndices();
+				List<Integer> rowIds = tableComponent.getRowIds();
+				ArrayList<Integer> rows = new ArrayList<Integer>(
+						selected.length);
+				selectedIndices.clear();
+				for (int i : selected) {
+					selectedIndices.add(i);
+					rows.add(rowIds.get(i));
+				}
+				tableComponent.setSelectedRows(rows);
+
+				// Enable/disable the remove action if the selection is
+				// non/empty.
+				removeRowAction.setEnabled(!event.getSelection().isEmpty());
+
+				return;
+			}
+		};
+		viewer.addSelectionChangedListener(listener);
+
 		return viewer;
+	}
+
+	/**
+	 * Creates the JFace {@code Action}s used to add or remove rows to or from
+	 * the {@link #tableComponent}.
+	 */
+	private void createActions() {
+
+		// Create the add action. It will add a new, default row.
+		addRowAction = new Action("+", Action.AS_PUSH_BUTTON) {
+			@Override
+			public void run() {
+				tableComponent.addRow();
+			}
+		};
+		addRowAction.setToolTipText("Adds a new, default row.");
+
+		// Create the remove action. It will remove all rows selected in the
+		// TableViewer from the TableComponent.
+		removeRowAction = new Action("-", Action.AS_PUSH_BUTTON) {
+			@Override
+			public void run() {
+				// The action should only be run once.
+				setEnabled(false);
+				// Delete all selected rows from the TableComponent.
+				for (int i = selectedIndices.size() - 1; i >= 0; i--) {
+					tableComponent.deleteRow(selectedIndices.get(i));
+				}
+				selectedIndices.clear();
+				// Make sure the model knows there is no selection.
+				tableComponent.setSelectedRows(new ArrayList<Integer>());
+			}
+		};
+		removeRowAction.setToolTipText("Removes all selected rows.");
+		removeRowAction.setEnabled(false);
+
+		return;
 	}
 
 }
