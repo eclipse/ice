f6479e375a03acaff42c5465868ebe45f3b50984
arbennett <bennettar@ornl.gov>
2015-01-21 22:14:51 -0800

    Adding a redraft of the CaebatKVPair item that reads in the case 6 key-value pair data by default.
    Signed-off-by: arbennett <bennettar@ornl.gov>

--- src/org.eclipse.ice.caebat/src/org/eclipse/ice/caebat/kvPair/CAEBATKVPairItem.java
+++ src/org.eclipse.ice.caebat/src/org/eclipse/ice/caebat/kvPair/CAEBATKVPairItem.java
@@ -12,23 +12,51 @@
  *******************************************************************************/
 package org.eclipse.ice.caebat.kvPair;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
 import javax.xml.bind.annotation.XmlRootElement;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.ice.datastructures.form.DataComponent;
 import org.eclipse.ice.datastructures.form.Entry;
+import org.eclipse.ice.datastructures.form.Form;
+import org.eclipse.ice.datastructures.form.MasterDetailsComponent;
+import org.eclipse.ice.datastructures.form.TableComponent;
+import org.eclipse.ice.io.ips.IPSReader;
+import org.eclipse.ice.io.serializable.IReader;
+import org.eclipse.ice.io.serializable.IWriter;
 import org.eclipse.ice.item.Item;
 
 /**
  * This class is a Model Item that configures a set of data components that
  * represent key-value pairs for CAEBAT input.
  * 
- * @author Jay Jay Billings
+ * @author Jay Jay Billings, Andrew Bennett
  * 
  */
 @XmlRootElement(name = "CAEBATKVPairItem")
-public class CAEBATKVPairItem extends Item {
+public class CAEBATKVPairItem extends Item implements IReader, IWriter{
 
+	/**
+	 * Keep track of everything that we can do with the KV Pair Item
+	 */
+	private ArrayList<String> actionItems;
+	
 	/**
 	 * The required constructor.
 	 * 
@@ -54,288 +82,12 @@ public class CAEBATKVPairItem extends Item {
 	 */
 	@Override
 	public void setupForm() {
+		// Create the form
+		form = new Form();
 
-		// Create the Form
-		super.setupForm();
-
-		// Create the data components
-		DataComponent thermalComponent = new DataComponent();
-		DataComponent electricalComponent = new DataComponent();
-		DataComponent electroChemComponent = new DataComponent();
-
-		// Setup the information about the thermal component
-		thermalComponent.setName("Thermal Properties");
-		thermalComponent.setDescription("Thermal properties and setttings");
-		thermalComponent.setId(1);
-
-		// Setup the information about the electrical component
-		electricalComponent.setName("Electrical Properties");
-		electricalComponent
-				.setDescription("Electrical properties and setttings");
-		electricalComponent.setId(2);
-
-		// Setup the information about the electroChem component
-		electroChemComponent.setName("Electrochemical Properties");
-		electroChemComponent
-				.setDescription("Electrochemical properties and setttings");
-		electroChemComponent.setId(3);
-
-		// Create a counter for the entry ids
-		int idCounter = 0;
-
-		// Setup the entries for the thermal component. There are no bounds or
-		// other information on these yet, so just let them have undefined type,
-		// no allowed values, etc.
-
-		// ELECTRICAL
-		Entry electricalEntry = new Entry() {
-			@Override
-			public void setup() {
-				setName("Electrical");
-				setTag("ELECTRICAL");
-				defaultValue = "1";
-			}
-		};
-		electricalEntry.setId(++idCounter);
-		// XCONDUCTIVITY
-		Entry xCondEntry = new Entry() {
-			@Override
-			public void setup() {
-				setName("X Conductivity");
-				setTag("XCONDUCTIVITY");
-				defaultValue = "1.7899";
-			}
-		};
-		xCondEntry.setId(++idCounter);
-		// YCONDUCTIVITY
-		Entry yCondEntry = new Entry() {
-			@Override
-			public void setup() {
-				setName("Y Conductivity");
-				setTag("YCONDUCTIVITY");
-				defaultValue = "1.7899";
-			}
-		};
-		yCondEntry.setId(++idCounter);
-		// ZCONDUCTIVITY
-		Entry zCondEntry = new Entry() {
-			@Override
-			public void setup() {
-				setName("Z Conductivity");
-				setTag("ZCONDUCTIVITY");
-				defaultValue = "1.7899";
-			}
-		};
-		zCondEntry.setId(++idCounter);
-		// HEATCAPACITY
-		Entry heatCapEntry = new Entry() {
-			@Override
-			public void setup() {
-				setName("Heat Capacity");
-				setTag("HEATCAPACITY");
-				defaultValue = "1100";
-			}
-		};
-		heatCapEntry.setId(++idCounter);
-		// ICSHORT
-		Entry icShortEntry = new Entry() {
-			@Override
-			public void setup() {
-				setName("IC Short");
-				setTag("ICSHORT");
-				defaultValue = "0";
-			}
-		};
-		icShortEntry.setId(++idCounter);
-		// NUMBCS
-		Entry numBCsEntry = new Entry() {
-			@Override
-			public void setup() {
-				setName("Number of Boundary Conditions");
-				setTag("NUMBCS");
-				defaultValue = "6";
-			}
-		};
-		numBCsEntry.setId(++idCounter);
-		// BCTYPE
-		Entry BCTypeEntry = new Entry() {
-			@Override
-			public void setup() {
-				setName("Boundary Condition Types");
-				setTag("BCTYPE");
-				defaultValue = "1,1,2,2,1,1";
-			}
-		};
-		BCTypeEntry.setId(++idCounter);
-		// BCIDS
-		Entry BCIdsEntry = new Entry() {
-			@Override
-			public void setup() {
-				setName("Boundary Condition Ids");
-				setTag("BCIDS");
-				defaultValue = "3,4,5,6,7,8";
-			}
-		};
-		BCIdsEntry.setId(++idCounter);
-		// AMBIENTTEMP
-		Entry ambientTempEntry = new Entry() {
-			@Override
-			public void setup() {
-				setName("Ambient Temperature");
-				setTag("AMBIENTTEMP");
-				defaultValue = "300,300,298,328,300,300";
-			}
-		};
-		ambientTempEntry.setId(++idCounter);
-		// CONVECTIVERATE
-		Entry convectiveRateEntry = new Entry() {
-			@Override
-			public void setup() {
-				setName("Convective Rate");
-				setTag("CONVECTIVERATE");
-				defaultValue = "0.0,0.0,5.0,5.0,1.0,1.0";
-			}
-		};
-		convectiveRateEntry.setId(++idCounter);
-
-		// Add the entries
-		thermalComponent.addEntry(electricalEntry);
-		thermalComponent.addEntry(xCondEntry);
-		thermalComponent.addEntry(yCondEntry);
-		thermalComponent.addEntry(zCondEntry);
-		thermalComponent.addEntry(heatCapEntry);
-		thermalComponent.addEntry(icShortEntry);
-		thermalComponent.addEntry(numBCsEntry);
-		thermalComponent.addEntry(BCTypeEntry);
-		thermalComponent.addEntry(BCIdsEntry);
-		thermalComponent.addEntry(ambientTempEntry);
-		thermalComponent.addEntry(convectiveRateEntry);
-
-		// Reset the counter for the next component;
-		idCounter = 0;
-
-		// Setup the entries for the electrical component. ICSHORT is reused
-		// from above.
-
-		// CurrentFlux
-		Entry currentFluxEntry = new Entry() {
-			@Override
-			public void setup() {
-				setName("Current Flux");
-				setTag("CurrentFlux");
-				defaultValue = "3602431";
-			}
-		};
-		currentFluxEntry.setId(++idCounter);
-
-		// Add the entries to the electrical component
-		electricalComponent.addEntry(currentFluxEntry);
-
-		// Reset the counter for the next component;
-		idCounter = 0;
-
-		// CRATE
-		Entry crateEntry = new Entry() {
-			@Override
-			public void setup() {
-				setName("CRATE");
-				defaultValue = "5";
-				setTag("CRATE");
-			}
-		};
-		crateEntry.setId(++idCounter);
-		// CURRDEN
-		Entry currentDensityEntry = new Entry() {
-			@Override
-			public void setup() {
-				setName("Current Density");
-				setTag("CURRDEN");
-				defaultValue = "79.26";
-			}
-		};
-		currentDensityEntry.setId(++idCounter);
-		// Cutoff
-		Entry cutoffEntry = new Entry() {
-			@Override
-			public void setup() {
-				setName("Cutoff");
-				setTag("CUTOFF");
-				defaultValue = "2.8";
-			}
-		};
-		cutoffEntry.setId(++idCounter);
-		// THICKNESS
-		Entry thicknessEntry = new Entry() {
-			@Override
-			public void setup() {
-				setName("Thickness");
-				setTag("THICKNESS");
-				defaultValue = "135e-6";
-			}
-		};
-		thicknessEntry.setId(++idCounter);
-		// YPolyCoefficients
-		Entry yPolyEntry = new Entry() {
-			@Override
-			public void setup() {
-				setName("Y Polynomial Coefficients");
-				setTag("YPolyCoefficients");
-				defaultValue = "3.6433e+02, -2.0629e+03, 1.1248e+04, -2.8978e+04, 4.0999e+04, -3.1353e+04, 9.8589e+03";
-			}
-		};
-		yPolyEntry.setId(++idCounter);
-		// UPolyCoefficients
-		Entry uPolyEntry = new Entry() {
-			@Override
-			public void setup() {
-				setName("U Polynomial Coefficients");
-				setTag("UPolyCoefficients");
-				defaultValue = "4.1210, -1.0206, -1.1902, 8.2938, -20.6735, 24.8684, -11.0422";
-			}
-		};
-		uPolyEntry.setId(++idCounter);
-		// YPolyDegrees
-		Entry yPolyDegEntry = new Entry() {
-			@Override
-			public void setup() {
-				setName("Y Polynomial Degrees of Freedom");
-				setTag("YPolyDegrees");
-				defaultValue = "6, 0";
-			}
-		};
-		yPolyDegEntry.setId(++idCounter);
-		// UPolyDegrees
-		Entry uPolyDegEntry = new Entry() {
-			@Override
-			public void setup() {
-				setName("U Polynomial Degrees of Freedom");
-				setTag("UPolyDegrees");
-				defaultValue = "6, 0";
-			}
-		};
-		uPolyDegEntry.setId(++idCounter);
-
-		// Add the entries to the electrochemical component
-		electroChemComponent.addEntry(crateEntry);
-		electroChemComponent.addEntry(currentDensityEntry);
-		electroChemComponent.addEntry(cutoffEntry);
-		electroChemComponent.addEntry(thicknessEntry);
-		electroChemComponent.addEntry(yPolyEntry);
-		electroChemComponent.addEntry(uPolyEntry);
-		electroChemComponent.addEntry(yPolyDegEntry);
-		electroChemComponent.addEntry(uPolyDegEntry);
-
-		// Add the components to the Form
-		form.addComponent(thermalComponent);
-		form.addComponent(electricalComponent);
-		form.addComponent(electroChemComponent);
-
-		// We only need the export to key-value pair process,
-		// so get rid of this one to conserve 'clicks'
-		// Did Emmy Noether ever speak of click conservation???
-		allowedActions.remove("Export to ICE Native Format");
-
-		return;
+		// If loading from the new item button we should just
+		// load up the default case 6 file by passing in null
+		loadInput(null);
 	}
 
 	/**
@@ -348,8 +100,210 @@ public class CAEBATKVPairItem extends Item {
 		setName(CAEBATKVPairBuilder.name);
 		itemType = CAEBATKVPairBuilder.type;
 		setItemBuilderName(CAEBATKVPairBuilder.name);
-		setDescription("A simple item to generate CAEBAT "
+		setDescription("An item to generate CAEBAT "
 				+ "key-value pair files.");
+		allowedActions.add(0, "Export to Key-Value Pair File");
+		actionItems = getAvailableActions();
+	}
+
+	/**
+	 * <p>
+	 * This operation loads the given example into the Form.
+	 * </p>
+	 * 
+	 * @param name
+	 *            The path name of the example file name to load.
+	 */
+	public void loadInput(String name) {
+
+		// If nothing is specified, load case 6 from inside the plugin
+		IFile inputFile = null;
+		File temp = null;
+		if (name == null) {
+			try {
+				// Path to the default file
+				String defaultFilePath = null;
+				// Create a filepath for the default file
+				if (project != null) {
+					defaultFilePath = project.getLocation().toOSString()
+							+ System.getProperty("file.separator")
+							+ "case_6.dat";
+				} else {
+					defaultFilePath = ResourcesPlugin.getWorkspace().getRoot()
+							.getLocation().toOSString()
+							+ System.getProperty("file.separator")
+							+ "case_6.dat";
+				}
+				
+				// Create a temporary location to load the default file
+				temp = new File(defaultFilePath);
+				if (!temp.exists()) {
+					temp.createNewFile();
+				}
+				
+				// Pull the default file from inside the plugin
+				URI uri = new URI(
+						"platform:/plugin/org.eclipse.ice.caebat/data/case_6.dat");
+				InputStream reader = uri.toURL().openStream();
+				FileOutputStream outStream = new FileOutputStream(temp);
+
+				// Write out the default file from the plugin to the temp location
+				int fileByte;
+				while ((fileByte = reader.read()) != -1) {
+					outStream.write(fileByte);
+				}
+				outStream.close();
+				inputFile = ResourcesPlugin.getWorkspace().getRoot()
+						.getFile(new Path(defaultFilePath));
+
+			} catch (URISyntaxException e) {
+				System.err
+						.println("CaebatKVPair Generator Message: Error!  Could not load the default"
+								+ " Caebat case data!");
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			// Load a custom file
+			String filePath = null;
+			// Get the path to where the file will be
+			if (project != null) {
+				filePath = project.getLocation().toOSString()
+						+ System.getProperty("file.separator") + name;
+			} else {
+				filePath = ResourcesPlugin.getWorkspace().getRoot()
+						.getLocation().toOSString()
+						+ System.getProperty("file.separator") + name;
+			}
+			// Get the file
+			inputFile = ResourcesPlugin.getWorkspace().getRoot()
+					.getFile(new Path(filePath));
+		}
+		
+		// Load the components from the file and setup the form
+		System.out.println("CaebatKVPair Generator Message: Loading" + inputFile.getFullPath().toOSString());
+
+		form = read(inputFile);
+		form.setName(getName());
+		form.setDescription(getDescription());
+		form.setId(getId());
+		form.setItemID(getId());
+		form.setActionList(actionItems);
+		
+		// Delete default file if it was copied into the workspace
+		if (temp != null) {
+			temp.delete();
+		}
+	}
+	
+	@Override
+	public Form read(IFile ifile) {
+		// Make sure there's something to look in
+		if (ifile == null) {
+			return null;
+		}
+		Form form = new Form();
+
+		// Read in the ini file to an ArrayList<String>
+		ArrayList<String> lines = new ArrayList<String>();
+		try {
+			// Read in the ini file and create the iterator
+			File file = new File(ifile.getFullPath().toOSString());
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+
+			// Read the FileInputStream and append to a StringBuffer
+			StringBuffer buffer = new StringBuffer();
+			int fileByte;
+			while ((fileByte = reader.read()) != -1) {
+				buffer.append((char) fileByte);
+			}
+
+			// Break up the StringBuffer at each newline character
+			String[] bufferSplit = (buffer.toString()).split("\n");
+			for (String each : bufferSplit) {
+				lines.add(each);
+			}
+
+			// Add a dummy EOF line so that the last line of the file is
+			// read in correctly
+			lines.add("EOF");
+		} catch (FileNotFoundException e) {
+			System.out
+					.println("CAEBATKVPair Generator Message: Error!  Could not find file for loading.");
+			return null;
+		} catch (IOException e) {
+			System.out
+					.println("CAEBATKVPair Generator Message: Error!  Trouble reading file.");
+			return null;
+		}
+
+		//
+		TableComponent kvTable = new TableComponent();
+		ArrayList<Entry> row;
+		ArrayList<Entry> kvEntries = new ArrayList<Entry>();
+		Entry key = new Entry();
+		Entry value = new Entry();
+		Entry kvEntry;
+		key.setName("Key");
+		value.setName("Value");
+		kvEntries.add(key);
+		kvEntries.add(value);
+		kvTable.setRowTemplate(kvEntries);
+		
+		for (String line : lines) {
+			if (line.contains("=")) {
+				String[] keyValue = line.split("=");
+				int rowID = kvTable.addRow();
+				row = kvTable.getRow(rowID);
+				row.get(0).setValue(keyValue[0]);
+				row.get(1).setValue(keyValue[1]);
+			}
+		}
+
+		form.addComponent(kvTable);
+		// Return the form
+		return form;
+	}
+
+	@Override
+	public void write(Form formToWrite, IFile file) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+	
+	@Override
+	public ArrayList<Entry> findAll(IFile file, String regex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	@Override
+	public void replace(IFile file, String regex, String value) {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * Return that this is a CAEBATKVPairItem
+	 */
+	@Override
+	public String getWriterType() {
+		return "CAEBATKVPairItem";
+	}
+
+	/**
+	 * Return that this is a CAEBATKVPairItem
+	 */
+	@Override
+	public String getReaderType() {
+		return "CAEBATKVPairItem";
 	}
 
 }
