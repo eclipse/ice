4062bd8b9d2f958ff725a15dfdd09842505db243
Jordan Deyton <deytonjh@ornl.gov>
2015-03-04 16:49:50 -0500

    Created IConnectionAdapter and IConnectionClient. Made ConnectionAdapter an implementation of IConnectionAdapter. Created a basic implementation of IConnectionClient (the code comes from the current version of VisItPlot).
    Signed-off-by: Jordan Deyton <deytonjh@ornl.gov>

--- src/org.eclipse.ice.viz.service/src/org/eclipse/ice/viz/service/connections/ConnectionAdapter.java
+++ src/org.eclipse.ice.viz.service/src/org/eclipse/ice/viz/service/connections/ConnectionAdapter.java
@@ -44,7 +44,8 @@ import org.eclipse.ice.datastructures.form.Entry;
  * @param <T>
  *            The type of the connection object.
  */
-public abstract class ConnectionAdapter<T> extends ICEObject {
+public abstract class ConnectionAdapter<T> extends ICEObject implements
+		IConnectionAdapter<T> {
 
 	/**
 	 * The current connection managed by this adapter.
@@ -71,6 +72,15 @@ public abstract class ConnectionAdapter<T> extends ICEObject {
 	}
 
 	// ---- Connect, Disconnect, and implementations. ---- //
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ice.viz.service.connections.IConnectionAdapter#connect()
+	 */
+	public boolean connect() {
+		return connect(false);
+	}
+
 	/**
 	 * Connects to the associated {@link #connection} if not already connected
 	 * or connecting.
@@ -159,6 +169,16 @@ public abstract class ConnectionAdapter<T> extends ICEObject {
 	 *         could not be opened.
 	 */
 	protected abstract T openConnection();
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ice.viz.service.connections.IConnectionAdapter#disconnect()
+	 */
+	public boolean disconnect() {
+		return disconnect(false);
+	}
 
 	/**
 	 * Disconnects the associated {@link #connection} if not already
@@ -297,59 +317,51 @@ public abstract class ConnectionAdapter<T> extends ICEObject {
 	}
 
 	// ---- Public Methods ---- //
-	/**
-	 * Gets the connection managed by this adapter.
+	/*
+	 * (non-Javadoc)
 	 * 
-	 * @return The associated connection.
+	 * @see
+	 * org.eclipse.ice.viz.service.connections.IConnectionAdapter#getConnection
+	 * ()
 	 */
 	public T getConnection() {
 		return connection;
 	}
 
-	/**
-	 * Gets the connection property corresponding to the specified key.
+	/*
+	 * (non-Javadoc)
 	 * 
-	 * @param key
-	 *            The key or ID of the required connection property.
-	 * @return The value of the connection property, or {@code null} if the key
-	 *         did not exist.
+	 * @see org.eclipse.ice.viz.service.connections.IConnectionAdapter#
+	 * getConnectionProperty(java.lang.String)
 	 */
 	public String getConnectionProperty(String key) {
 		return connectionProperties.get(key);
 	}
 
-	/**
-	 * Gets the key currently associated with this connection. The value is
-	 * usually maintained by a connection manager.
+	/*
+	 * (non-Javadoc)
 	 * 
-	 * @return The connection key.
+	 * @see org.eclipse.ice.viz.service.connections.IConnectionAdapter#getKey()
 	 */
 	public String getKey() {
 		return getName();
 	}
 
-	/**
-	 * Gets the current state of the associated connection.
+	/*
+	 * (non-Javadoc)
 	 * 
-	 * @return The current state of the associated connection.
+	 * @see
+	 * org.eclipse.ice.viz.service.connections.IConnectionAdapter#getState()
 	 */
 	public ConnectionState getState() {
 		return state;
 	}
 
-	/**
-	 * Sets the connection's required {@link #connectionProperties properties}
-	 * based on the provided list of {@code Entry}s (usually a row from a
-	 * {@link ConnectionManager}).
-	 * <p>
-	 * The associated connection will <i>not</i> be reset if the connection
-	 * properties have changed.
-	 * </p>
+	/*
+	 * (non-Javadoc)
 	 * 
-	 * @param properties
-	 *            The list of new connection properties.
-	 * @return True if the new connection properties were valid and a change
-	 *         occurred, false otherwise.
+	 * @see org.eclipse.ice.viz.service.connections.IConnectionAdapter#
+	 * setConnectionProperties(java.util.List)
 	 */
 	public abstract boolean setConnectionProperties(List<Entry> properties);
 
---
+++ src/org.eclipse.ice.viz.service/src/org/eclipse/ice/viz/service/connections/IConnectionClient.java
@@ -0,0 +1,32 @@
+package org.eclipse.ice.viz.service.connections;
+
+import org.eclipse.ice.datastructures.ICEObject.IUpdateableListener;
+
+/**
+ * A connection client is a class that can be associated with a single
+ * {@link IConnectionAdapter}. It registers with the adapter as an
+ * {@link IUpdateableListener} and is notified when the connection changes via
+ * its {@link #update(IUpdateable)} method.
+ * 
+ * @author Jordan Deyton
+ *
+ * @param <T>
+ *            The connection object's type.
+ */
+public interface IConnectionClient<T> extends IUpdateableListener {
+
+	/**
+	 * Sets the current connection associated with the client.
+	 * <p>
+	 * <b>Note:</b> Implementations should at least unregister from the
+	 * previously associated connection and register with the new one. It may
+	 * also trigger an update to the client.
+	 * </p>
+	 * 
+	 * @param adapter
+	 *            The new connection adapter. If {@code null}, the connection
+	 *            will be unset and the plot will be cleared.
+	 */
+	void setConnection(IConnectionAdapter<T> adapter);
+
+}
