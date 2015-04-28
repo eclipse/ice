cb679f529091d62c03a79015683d33d2a5a48594
Jordan Deyton <deytonjh@ornl.gov>
2015-04-28 18:26:07 -0400

    Added the skeletons for test code. This is for the connection-based infrastructure used by the VisIt and ParaView viz services.
    Signed-off-by: Jordan Deyton <deytonjh@ornl.gov>

---
+++ tests/org.eclipse.ice.viz.service.test/src/org/eclipse/ice/viz/service/connections/test/ConnectionPlotRenderTester.java
@@ -0,0 +1,19 @@
+package org.eclipse.ice.viz.service.connections.test;
+
+import org.eclipse.ice.viz.service.connections.ConnectionPlotRender;
+import org.junit.Ignore;
+
+/**
+ * This class checks the {@link ConnectionPlotRender}'s default widgets and that
+ * the sub-class is called for further decoration of plot widgets at the
+ * appropriate times.
+ * 
+ * @author Jordan Deyton
+ *
+ */
+@Ignore
+public class ConnectionPlotRenderTester {
+
+	// TODO Figure out how to test this class...
+
+}
---
+++ tests/org.eclipse.ice.viz.service.test/src/org/eclipse/ice/viz/service/connections/test/ConnectionPlotTester.java
@@ -0,0 +1,49 @@
+package org.eclipse.ice.viz.service.connections.test;
+
+import org.eclipse.ice.viz.service.connections.ConnectionPlot;
+import org.eclipse.ice.viz.service.connections.ConnectionPlotRender;
+import org.eclipse.ice.viz.service.connections.IConnectionAdapter;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * This class checks that the {@link ConnectionPlot} correctly synchronizes the
+ * associated {@link ConnectionPlotRender} with changes to its properties or to
+ * its associated {@link IConnectionAdapter}.
+ * 
+ * @author Jordan Deyton
+ *
+ */
+@Ignore
+public class ConnectionPlotTester {
+
+	// TODO Implement these tests.
+
+	/**
+	 * This test checks the default state of a {@code ConnectionPlot} after
+	 * being constructed.
+	 */
+	@Test
+	public void checkConstruction() {
+
+	}
+
+	/**
+	 * Tests {@code ConnectionPlot}'s response to updating the plot's data
+	 * source URI.
+	 */
+	@Test
+	public void checkDataSource() {
+
+	}
+
+	/**
+	 * Tests the {@code IConnectionAdapter} associated with a
+	 * {@code ConnectionPlot}.
+	 */
+	@Test
+	public void checkConnectionAdapter() {
+
+	}
+
+}
