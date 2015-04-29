c1be6e086b6c14c78446f750d3083d1114d0e503
Jordan Deyton <deytonjh@ornl.gov>
2015-04-29 11:41:15 -0400

    Implemented the SecretEntryTester and fixed an obscure bug in SecretEntry.
    Signed-off-by: Jordan Deyton <deytonjh@ornl.gov>

--- tests/org.eclipse.ice.viz.service.test/src/org/eclipse/ice/viz/service/connections/test/SecretEntryTester.java
+++ tests/org.eclipse.ice.viz.service.test/src/org/eclipse/ice/viz/service/connections/test/SecretEntryTester.java
@@ -11,8 +11,14 @@
  *******************************************************************************/
 package org.eclipse.ice.viz.service.connections.test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertTrue;
+
+import org.eclipse.ice.datastructures.form.BasicEntryContentProvider;
+import org.eclipse.ice.datastructures.form.Entry;
+import org.eclipse.ice.datastructures.form.IEntryContentProvider;
 import org.eclipse.ice.viz.service.connections.SecretEntry;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -22,10 +28,7 @@ import org.junit.Test;
  * @author Jordan Deyton
  *
  */
-@Ignore
 public class SecretEntryTester {
-
-	// TODO Implement
 
 	/**
 	 * Checks that the secret flag is properly set at construction.
@@ -33,6 +36,21 @@ public class SecretEntryTester {
 	@Test
 	public void checkConstruction() {
 
+		SecretEntry entry;
+
+		// Make sure the secret flag is set for the nullary constructor.
+		entry = new SecretEntry();
+		assertTrue(entry.isSecret());
+		assertTrue(entry.isSecret());
+
+		// Make sure the secret flag is set when the IEntryContentProvider is
+		// passed into the constructor.
+		IEntryContentProvider contentProvider = new BasicEntryContentProvider();
+		entry = new SecretEntry(contentProvider);
+		assertTrue(entry.isSecret());
+		assertTrue(entry.isSecret());
+
+		return;
 	}
 
 	/**
@@ -40,6 +58,38 @@ public class SecretEntryTester {
 	 */
 	@Test
 	public void checkCopy() {
+		SecretEntry entry;
+		SecretEntry copy;
+		Object clone;
 
+		// Create the initial Entry that will be copied and cloned later.
+		entry = new SecretEntry();
+		entry.setName("Adam Gibson");
+		entry.setDescription("There's someone in my house, "
+				+ "eating my birthday cake, with my family, and it's not me!");
+		// Double-check that its secret flag is set.
+		assertTrue(entry.isSecret());
+
+		// Copy it and it should still be equal.
+		copy = new SecretEntry();
+		copy.copy(entry);
+		// Check that the copy's secret flag is set.
+		assertTrue(copy.isSecret());
+		// They should also be equal (but different references).
+		assertNotSame(entry, copy);
+		assertEquals(entry, copy);
+		assertEquals(copy, entry);
+
+		// Clone it and it should be an equivalent *SecretEntry*.
+		clone = entry.clone();
+		assertTrue(clone instanceof SecretEntry);
+		// Check that the clone's secret flag is set.
+		assertTrue(((Entry) clone).isSecret());
+		// They should also be equal (but different references).
+		assertNotSame(entry, clone);
+		assertEquals(entry, clone);
+		assertEquals(clone, entry);
+
+		return;
 	}
 }
