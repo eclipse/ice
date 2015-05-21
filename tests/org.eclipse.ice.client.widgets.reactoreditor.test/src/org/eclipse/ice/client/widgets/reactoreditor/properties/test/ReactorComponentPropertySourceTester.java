c87e7339f7e8aa9e82993c8ccfce1c2818b100d7
Jordan Deyton <deytonjh@ornl.gov>
2015-05-21 17:12:42 -0400

    Made the map of properties private final. It was protected so that it could be accessed from a subclass in the tester, but I just noticed that's not really necessary. I refactored the tester to test the exposed properties without access to the underlying map.
    Signed-off-by: Jordan Deyton <deytonjh@ornl.gov>

--- tests/org.eclipse.ice.client.widgets.reactoreditor.test/src/org/eclipse/ice/client/widgets/reactoreditor/properties/test/ReactorComponentPropertySourceTester.java
+++ tests/org.eclipse.ice.client.widgets.reactoreditor.test/src/org/eclipse/ice/client/widgets/reactoreditor/properties/test/ReactorComponentPropertySourceTester.java
@@ -16,19 +16,17 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.ice.client.widgets.reactoreditor.properties.ReactorComponentPropertySource;
 import org.eclipse.ice.client.widgets.reactoreditor.properties.SimpleProperty;
 import org.eclipse.ice.datastructures.ICEObject.ICEObject;
 import org.eclipse.ice.datastructures.componentVisitor.IComponentVisitor;
 import org.eclipse.ice.datastructures.componentVisitor.IReactorComponent;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 import org.eclipse.ui.views.properties.IPropertyDescriptor;
 import org.eclipse.ui.views.properties.PropertyDescriptor;
 import org.junit.Test;
@@ -70,24 +68,6 @@ public class ReactorComponentPropertySourceTester {
 	}
 
 	/**
-	 * Sets up the List of basic properties for IReactorComponents.
-	 */
-	private List<SimpleProperty> getBaseProperties(IReactorComponent component) {
-		// Initialize the list and add the basic properties.
-		List<SimpleProperty> properties = new ArrayList<SimpleProperty>();
-
-		String category = "Basic Properties";
-		properties.add(new SimpleProperty("name", "Name", category, component
-				.getName()));
-		properties.add(new SimpleProperty("description", "Description",
-				category, component.getDescription()));
-		properties.add(new SimpleProperty("id", "ID", category, component
-				.getId()));
-
-		return properties;
-	}
-
-	/**
 	 * Checks the construction of an ReactorComponentPropertySource with valid
 	 * and invalid parameters.
 	 */
@@ -96,60 +76,29 @@ public class ReactorComponentPropertySourceTester {
 
 		// Initialize a property source.
 		TestReactorComponent component;
-		List<SimpleProperty> properties;
-		Map<String, SimpleProperty> propertyMap;
-		ReactorComponentProperties source;
+		ReactorComponentPropertySource source;
 
-		// Check invalid constructor arguments.
-		component = new TestReactorComponent();
-		component.setName("Invalid reactor component!");
-		component.setDescription("This is an invalid reactor component!");
-		properties = getBaseProperties(component);
-
-		source = new ReactorComponentProperties(null);
-		propertyMap = source.getPropertyMap();
-
-		// Make sure all the basic properties are correct.
-		for (SimpleProperty property : properties) {
-			SimpleProperty p = propertyMap.get(property.getId());
-			assertNotNull(p);
-
-			assertEquals(property.getId(), p.getId());
-			assertEquals(property.getValue(), p.getValue());
-
-			PropertyDescriptor descriptor = property.getPropertyDescriptor();
-			PropertyDescriptor d = p.getPropertyDescriptor();
-
-			assertEquals(descriptor.getId(), d.getId());
-			assertEquals(descriptor.getDisplayName(), d.getDisplayName());
-			assertEquals(descriptor.getCategory(), d.getCategory());
-		}
+		// Check the default name, description, and id for *null* arguments.
+		source = new ReactorComponentPropertySource(null);
+		assertEquals("Invalid reactor component!",
+				source.getPropertyValue("name"));
+		assertEquals("This is an invalid reactor component!",
+				source.getPropertyValue("description"));
+		assertEquals(1, source.getPropertyValue("id"));
 
 		// Check valid constructor arguments.
 		component = new TestReactorComponent();
 		component.setName("A valid reactor component.");
 		component.setId(14132);
 		component.setDescription("Valid valid valid valid valid.");
-		properties = getBaseProperties(component);
 
-		source = new ReactorComponentProperties(component);
-		propertyMap = source.getPropertyMap();
-
-		// Make sure all the basic properties are correct.
-		for (SimpleProperty property : properties) {
-			SimpleProperty p = propertyMap.get(property.getId());
-			assertNotNull(p);
-
-			assertEquals(property.getId(), p.getId());
-			assertEquals(property.getValue(), p.getValue());
-
-			PropertyDescriptor descriptor = property.getPropertyDescriptor();
-			PropertyDescriptor d = p.getPropertyDescriptor();
-
-			assertEquals(descriptor.getId(), d.getId());
-			assertEquals(descriptor.getDisplayName(), d.getDisplayName());
-			assertEquals(descriptor.getCategory(), d.getCategory());
-		}
+		// Check the default property values for the valid component.
+		source = new ReactorComponentPropertySource(component);
+		assertEquals("A valid reactor component.",
+				source.getPropertyValue("name"));
+		assertEquals("Valid valid valid valid valid.",
+				source.getPropertyValue("description"));
+		assertEquals(14132, source.getPropertyValue("id"));
 
 		return;
 	}
@@ -162,73 +111,71 @@ public class ReactorComponentPropertySourceTester {
 	public void checkAddProperty() {
 
 		// Initialize a property source.
-		TestReactorComponent component;
-		List<SimpleProperty> properties;
-		Map<String, SimpleProperty> propertyMap;
-		ReactorComponentProperties source;
+		TestReactorComponent component = new TestReactorComponent();
+		ReactorComponentPropertySource source = new ReactorComponentPropertySource(
+				component);
 
-		component = new TestReactorComponent();
-		properties = getBaseProperties(component);
+		IPropertyDescriptor descriptor;
 
-		source = new ReactorComponentProperties(component);
-		propertyMap = source.getPropertyMap();
+		// We start off with 3 properties: component name, description, and ID.
+		int properties = 3;
 
 		// Add some properties and test.
 
-		// Add a new property. The return value should be true and the property
-		// should be in the Map.
+		// Add a new property. The return value should be true, and we should be
+		// able to fetch the property.
 		SimpleProperty property1 = new SimpleProperty("same id", "heyo",
 				"Potent Potables", 1000);
 		assertTrue(source.addProperty(property1));
-		assertSame(property1, propertyMap.get(property1.getId()));
+		// Check the property value.
+		assertEquals(1000, source.getPropertyValue("same id"));
+		// Check the size of the property descriptors.
+		assertEquals(++properties, source.getPropertyDescriptors().length);
+		// Check the property descriptor. The ID is checked in the find method.
+		descriptor = findPropertyDescriptor(source.getPropertyDescriptors(),
+				"same id");
+		assertNotNull(descriptor);
+		assertEquals("heyo", descriptor.getDisplayName());
+		assertEquals("Potent Potables", descriptor.getCategory());
 
 		// Add a new property with the same ID. The return value should be false
 		// and the new property shouldn't be in the Map.
 		SimpleProperty property2 = new SimpleProperty("same id", "nerf",
 				"Potent Potables", "orange, plastic, toy gun");
 		assertFalse(source.addProperty(property2));
-		assertSame(property1, propertyMap.get(property2.getId()));
+		// Check the size of the property descriptors. Should not have changed.
+		assertEquals(properties, source.getPropertyDescriptors().length);
+		// Check the property value. This should not have changed.
+		assertEquals(1000, source.getPropertyValue("same id"));
+		// Check the property descriptor. This should not have changed.
+		descriptor = findPropertyDescriptor(source.getPropertyDescriptors(),
+				"same id");
+		assertNotNull(descriptor);
+		assertEquals("heyo", descriptor.getDisplayName());
+		assertEquals("Potent Potables", descriptor.getCategory());
 
 		// Add a new property. The return value should be true and the property
 		// should be in the Map.
 		property2 = new SimpleProperty("new id", "nerf", "Potent Potables",
 				"orange, plastic, toy gun");
 		assertTrue(source.addProperty(property2));
-		assertSame(property2, propertyMap.get(property2.getId()));
+		// Check the size of the property descriptors.
+		assertEquals(++properties, source.getPropertyDescriptors().length);
+		// Check the property value.
+		assertEquals("orange, plastic, toy gun",
+				source.getPropertyValue("new id"));
+		// Check the property descriptor. The ID is checked in the find method.
+		descriptor = findPropertyDescriptor(source.getPropertyDescriptors(),
+				"new id");
+		assertNotNull(descriptor);
+		assertEquals("nerf", descriptor.getDisplayName());
+		assertEquals("Potent Potables", descriptor.getCategory());
 
-		// Check all the properties from getPropertyDescriptors.
-		// Make sure all the basic properties are correct.
-		properties.add(property1);
-		properties.add(property2);
-		IPropertyDescriptor[] descriptors = source.getPropertyDescriptors();
-		for (SimpleProperty property : properties) {
-
-			IPropertyDescriptor descriptor = null;
-			for (int i = 0; i < descriptors.length && descriptor == null; i++) {
-				if (property.getId().equals(descriptors[i].getId())) {
-					descriptor = descriptors[i];
-				}
-			}
-			// Make sure a corresponding descriptor was found.
-			if (descriptor == null) {
-				fail();
-			}
-
-			// Check the descriptor against the expected base property's
-			// descriptor.
-			PropertyDescriptor baseDescriptor = property
-					.getPropertyDescriptor();
-			assertEquals(baseDescriptor.getId(), descriptor.getId());
-			assertEquals(baseDescriptor.getDisplayName(),
-					descriptor.getDisplayName());
-			assertEquals(baseDescriptor.getCategory(), descriptor.getCategory());
-
-			Object id = property.getId();
-			Object value = property.getValue();
-
-			// Check the value.
-			assertEquals(value, source.getPropertyValue(id));
-		}
+		// Check the default properties.
+		assertEquals(component.getName(), source.getPropertyValue("name"));
+		assertEquals(component.getDescription(),
+				source.getPropertyValue("description"));
+		assertEquals(component.getId(), source.getPropertyValue("id"));
 
 		return;
 	}
@@ -298,35 +245,44 @@ public class ReactorComponentPropertySourceTester {
 	}
 
 	/**
-	 * This class is for testing only. It exposes the underlying Map of
-	 * SimpleProperties from ReactorComponentPropertySource.
+	 * Loops over the provided array of descriptors until one is found whose ID
+	 * matches the provided ID.
 	 * 
-	 * @author Jordan H. Deyton
-	 * 
+	 * @param descriptors
+	 *            The array of descriptors to search.
+	 * @param id
+	 *            The ID of the desired property descriptor.
+	 * @return The property descriptor whose ID matches the provided ID, or
+	 *         {@code null} if it could not be found.
 	 */
-	private class ReactorComponentProperties extends
-			ReactorComponentPropertySource {
-
-		/**
-		 * The default constructor.
-		 * 
-		 * @param component
-		 *            The IReactorComponent to pass to the
-		 *            ReactorComponentPropertySource.
-		 */
-		public ReactorComponentProperties(IReactorComponent component) {
-			super(component);
-
-			return;
+	private IPropertyDescriptor findPropertyDescriptor(
+			IPropertyDescriptor[] descriptors, Object id) {
+		IPropertyDescriptor descriptor = null;
+		for (int i = 0; i < descriptors.length && descriptor == null; i++) {
+			IPropertyDescriptor tmp = descriptors[i];
+			if (id.equals(tmp.getId())) {
+				descriptor = tmp;
+			}
 		}
-
-		/**
-		 * Gets the Map of SimpleProperty instances keyed on their ID.
-		 * 
-		 * @return A Map of SimpleProperty instances.
-		 */
-		public Map<String, SimpleProperty> getPropertyMap() {
-			return properties;
-		}
+		return descriptor;
 	}
+
+	/**
+	 * Sets up the List of basic properties for IReactorComponents.
+	 */
+	private List<SimpleProperty> getBaseProperties(IReactorComponent component) {
+		// Initialize the list and add the basic properties.
+		List<SimpleProperty> properties = new ArrayList<SimpleProperty>();
+
+		String category = "Basic Properties";
+		properties.add(new SimpleProperty("name", "Name", category, component
+				.getName()));
+		properties.add(new SimpleProperty("description", "Description",
+				category, component.getDescription()));
+		properties.add(new SimpleProperty("id", "ID", category, component
+				.getId()));
+
+		return properties;
+	}
+
 }
