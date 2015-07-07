1f791dd1ba3ac1f5f31b165c4e5c5a4800dc1bc2
Alex McCaskey <mccaskeyaj@ornl.gov>
2015-07-07 16:04:25 -0400

    PTP New Connection for FormStatus.NeedsInfo
    Committing code that presents the user with a New Connection wizard from
    PTP if the provided Form represents remote launch credentials, otherwise
    it just presents the usual ExtraInfoDialog.
    
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- src/org.eclipse.ice.client.widgets/src/org/eclipse/ice/client/widgets/EclipseExtraInfoWidget.java
+++ src/org.eclipse.ice.client.widgets/src/org/eclipse/ice/client/widgets/EclipseExtraInfoWidget.java
@@ -18,6 +18,11 @@ import org.eclipse.ice.datastructures.form.DataComponent;
 import org.eclipse.ice.datastructures.form.Form;
 import org.eclipse.ice.iclient.uiwidgets.IExtraInfoWidget;
 import org.eclipse.ice.iclient.uiwidgets.IWidgetClosedListener;
+import org.eclipse.remote.core.IRemoteConnectionType;
+import org.eclipse.remote.core.IRemoteConnectionWorkingCopy;
+import org.eclipse.remote.core.IRemoteServicesManager;
+import org.eclipse.remote.ui.IRemoteUIConnectionService;
+import org.eclipse.remote.ui.IRemoteUIConnectionWizard;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
@@ -60,6 +65,11 @@ public class EclipseExtraInfoWidget implements IExtraInfoWidget {
 	private ArrayList<IWidgetClosedListener> listeners;
 
 	/**
+	 * Reference to the RemoteServicesManager provided by the platform
+	 */
+	private static IRemoteServicesManager remoteManager;
+
+	/**
 	 * <p>
 	 * The constructor
 	 * </p>
@@ -95,19 +105,68 @@ public class EclipseExtraInfoWidget implements IExtraInfoWidget {
 				display = PlatformUI.getWorkbench().getDisplay();
 				shell = display.getActiveShell();
 
-				// Instantiate the dialog, set the DataComponent and set the
-				// listeners
-				infoDialog = new ExtraInfoDialog(shell);
-				infoDialog.setDataComponent((DataComponent) iceForm
-						.getComponents().get(0));
-				for (IWidgetClosedListener i : listeners) {
-					infoDialog.setCloseListener(i);
+				// Get whether this Extra Info Form corresponds to 
+				// a remote host and needs a remote connection
+				boolean isLaunchForm = "Log-in Information".equals(iceForm.getComponents().get(0).getName());
+				
+				// If it is, then let's use the remote interfaces from PTP
+				if (isLaunchForm) {
+					
+					// Get the default SSH connection type
+					IRemoteConnectionType type = remoteManager.getRemoteConnectionTypes().get(0);
+					
+					// Get the UI connection services which can give us a remote connection wizard
+					IRemoteUIConnectionService connectionService = type.getService(IRemoteUIConnectionService.class);
+					
+					// Get the Wizard!
+					IRemoteUIConnectionWizard wizard = connectionService.getConnectionWizard(shell);
+					if (wizard != null) {
+						// Set the connection name
+						wizard.setConnectionName("New Connection");
+						
+						// Open the wizard and get whether the input was valid
+						IRemoteConnectionWorkingCopy copy = wizard.open();
+						if (copy == null) {
+							for (IWidgetClosedListener i : listeners) {
+								i.cancelled();
+							}
+						} else {
+							for (IWidgetClosedListener i : listeners) {
+								i.closedOK();
+							}
+						}
+					}
+				} else {
+					// If this is not a remote launch, then just use 
+					// the regular ExtraInfoDialog.
+					
+					// Instantiate the dialog, set the DataComponent and set the
+					// listeners
+					infoDialog = new ExtraInfoDialog(shell);
+					infoDialog.setDataComponent((DataComponent) iceForm.getComponents().get(0));
+					// Open the dialog
+					infoDialog.open();
+					for (IWidgetClosedListener i : listeners) {
+						infoDialog.setCloseListener(i);
+					}
 				}
-				// Open the dialog
-				infoDialog.open();
 			}
 		});
 
+	}
+
+	/**
+	 * This method is used by the platform to give this MOOSEModel a reference
+	 * to the available IRemoteServicesManager.
+	 * 
+	 * @param manager
+	 */
+	public void setRemoteServicesManager(IRemoteServicesManager manager) {
+		if (manager != null) {
+			System.out.println(
+					"[EclipseExtraInfoWidget Message] Setting the IRemoteServicesManager: " + manager.toString());
+			remoteManager = manager;
+		}
 	}
 
 	/**
