e509be35ade0b67586ef9ebe0f2cfa5e52ec9161
Jordan Deyton <deytonjh@ornl.gov>
2015-08-04 09:44:16 -0400

    Adding an tester that needs to be implemented to viz.test bundle.
    This is to prevent Eclipse from hiding the src folder then complain that
    it does not exist. Also, this is for a class that should be tested.
    
    Signed-off-by: Jordan Deyton <deytonjh@ornl.gov>

---
+++ tests/org.eclipse.ice.viz.test/src/org/eclipse/ice/viz/test/PlotEntryContentProviderTester.java
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2015 UT-Battelle, LLC.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Jordan Deyton - Initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.ice.viz.test;
+
+public class PlotEntryContentProviderTester {
+
+	// TODO Add tests for the PlotEntryContentProvider
+	
+}
