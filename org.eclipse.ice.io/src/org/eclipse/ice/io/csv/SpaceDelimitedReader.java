b74ea3d7b3fc4bdb1f99c9a1d19f966e866bc9c6
Jay Jay Billings <billingsjj@ornl.gov>
2015-10-29 02:44:57 +0000

    Added support for space-based delimited text.
    Added a base class for delimited text. Added a space-based delimited
    text file reader. Fixed the reflectivity model to use the IOService and
    to use space-based delimiters.
    
    Signed-off-by: Jay Jay Billings <billingsjj@ornl.gov>

--- org.eclipse.ice.io/src/org/eclipse/ice/io/csv/SpaceDelimitedReader.java
+++ org.eclipse.ice.io/src/org/eclipse/ice/io/csv/SpaceDelimitedReader.java
@@ -12,25 +12,26 @@
 package org.eclipse.ice.io.csv;
 
 /**
- * This class implements the IReader interface to provide a reader for CSV
- * files. It can read any well-formed CSV file. It stores its results in a
- * ListComponent<String[]> on the Form returned from read(). Each String [] in
- * the ListComponent is a line of the file, split and trimmed but uncast.
- * Clients must know the concrete type to which they want to cast.
+ * This class implements the IReader interface to provide a reader for space
+ * delimited text files. It can read any well-formed space delimited text file.
+ * It stores its results in a ListComponent<String[]> on the Form returned from
+ * read(). Each String [] in the ListComponent is a line of the file, split and
+ * trimmed but uncast. Clients must know the concrete type to which they want to
+ * cast.
  *
  * Comments are ignored and begin with the "#" character.
  *
  * @author Jay Jay Billings
  *
  */
-public class CSVReader extends DelimitedReader {
+public class SpaceDelimitedReader extends DelimitedReader {
 
 	/**
 	 * The constructor
 	 */
-	public CSVReader() {
-		// Set the delimiter and type
-		delimiter = ",";
-		type = "csv";
+	public SpaceDelimitedReader() {
+		// Set the file type. The delimiter is already a space by default.
+		type = "space-delimited";
 	}
+
 }
