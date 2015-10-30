c1a257d0b9926ed76c69d14e5f476ec23fbc494b
Menghan Li <menghanli723@gmail.com>
2015-10-30 14:50:04 -0400

    Bug 478471
    Created a IFormWidgetBuilder.getFormWidgetBuilders()operation.
    
    I made this operation to be a static interface operation on
    IFormWidgetBuilder for getting list of registered extensions of that
    interface.
    
    Signed-off-by: Menghan Li <menghanli723@gmail.com>

--- org.eclipse.ice.client.widgets/src/org/eclipse/ice/client/widgets/IFormWidgetBuilder.java
+++ org.eclipse.ice.client.widgets/src/org/eclipse/ice/client/widgets/IFormWidgetBuilder.java
@@ -12,7 +12,14 @@
 *******************************************************************************/
 package org.eclipse.ice.client.widgets;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtensionPoint;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.ice.iclient.uiwidgets.IFormWidget;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /** 
  * <p>This interface can be realized by client developers to customize the way that the EclipseUIWidgetFactory renders Forms for the user. Realizations of this interface should be registered as an OSGi Declarative service.</p><p>IFormWidgetBuilders must provide the name of the Form that they are meant to render. This requires that a Form with that name exist for the IFormWidget that the Builder creates to work properly. Thus, if no Form with the target name does not exist, the Builder should never be called.</p>
@@ -30,4 +37,41 @@ public interface IFormWidgetBuilder {
 	 * @return <p>The specialized IFormWidget from the service.</p>
 	 */
 	public IFormWidget build();
+	
+
+/**
+	 * This operation retrieves all of the IFormWidgetBuilders from the
+	 * ExtensionRegistry.
+	 *
+	 * @return The array of IFormWidgetBuilders that were found in the registry.
+	 * @throws CoreException
+	 *             This exception is thrown if an extension cannot be loaded.
+	 */
+public static IFormWidgetBuilder[] getFormWidgetBuilders() throws CoreException {
+
+	/**
+	 * Logger for handling event messages and other information.
+	 */
+	Logger logger = LoggerFactory.getLogger(IFormWidgetBuilder.class);
+
+	IFormWidgetBuilder [] builders = null;
+	String id = "org.eclipse.ice.item.itemBuilder";
+	IExtensionPoint point = Platform.getExtensionRegistry()
+			.getExtensionPoint(id);
+		
+		// If the point is available, create all the builders and load them into
+		// the array.
+		if (point != null) {
+			IConfigurationElement[] elements = point.getConfigurationElements();
+			builders = new IFormWidgetBuilder[elements.length];
+			for (int i = 0; i < elements.length; i++) {
+				builders[i] = (IFormWidgetBuilder) elements[i]
+						.createExecutableExtension("class");
+			}
+		} else {
+			logger.error("Extension Point " + id + "does not exist");
+		}
+
+		return builders;
+	}
 }
