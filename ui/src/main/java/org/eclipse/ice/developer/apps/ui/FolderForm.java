4b786ed5094d3d6cb50d8e71326fb4d2b9db2e5e
Anara Kozhokanova <anara.ornl@gmail.com>
2017-03-06 16:10:19 -0500

    Add advanced view for the Docker option.
    Signed-off-by: Anara Kozhokanova anara.ornl@gmail.com

---
+++ ui/src/main/java/org/eclipse/ice/developer/apps/ui/FolderForm.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.eclipse.ice.developer.apps.ui;
+
+/**
+ * @author kfu
+ *
+ */
+public class FolderForm {
+
+	/**
+	 * 
+	 */
+	public FolderForm() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
