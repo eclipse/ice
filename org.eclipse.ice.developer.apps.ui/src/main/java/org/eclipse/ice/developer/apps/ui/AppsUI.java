18eadb306a36b51d6e430fa99af11f16c7a76dff
Jay Jay Billings <billingsjj@ornl.gov>
2017-03-14 13:46:10 -0400

    Enabled Developer UI bundle to work in OSGI with Jetty.
    Signed-off-by: Jay Jay Billings <billingsjj@ornl.gov>

--- org.eclipse.ice.developer.apps.ui/src/main/java/org/eclipse/ice/developer/apps/ui/AppsUI.java
+++ org.eclipse.ice.developer.apps.ui/src/main/java/org/eclipse/ice/developer/apps/ui/AppsUI.java
@@ -1,19 +1,32 @@
 package org.eclipse.ice.developer.apps.ui;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import org.eclipse.core.runtime.FileLocator;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.FontAwesome;
@@ -33,27 +46,35 @@ import com.vaadin.ui.VerticalLayout;
 
 import apps.AppsFactory;
 import apps.EnvironmentManager;
-import apps.IEnvironment;
-import apps.impl.SpackPackageImpl;
 
 /**
- * This UI is the application entry point. A UI may either represent a browser window 
- * (or tab) or some part of a html page where a Vaadin application is embedded.
+ * This UI is the application entry point. A UI may either represent a browser
+ * window (or tab) or some part of a html page where a Vaadin application is
+ * embedded.
  * <p>
- * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
- * overridden to add component to the user interface and initialize non-component functionality.
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is
+ * intended to be overridden to add component to the user interface and
+ * initialize non-component functionality.
  */
-@Theme("apps-theme")
+@Theme("mytheme")
 public class AppsUI extends UI {
 
-	
-	
+	/**
+	 * A serialization ID - if you remove this, OSGI DS will fail!
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * The HttpService Reference for the OSGI framework.
+	 */
+	static private HttpService httpService;
+
 	private VerticalLayout mainLayout = new VerticalLayout();
-	private HorizontalLayout packagesLayout = new  HorizontalLayout();
-	private HorizontalLayout gitAndOsBtnsLayout = new  HorizontalLayout();
-	private HorizontalLayout cnlAndValBtnsLayout = new  HorizontalLayout();
-	private HorizontalLayout advancedLayout = new  HorizontalLayout();
-	private VerticalLayout envLayout = new  VerticalLayout();
+	private HorizontalLayout packagesLayout = new HorizontalLayout();
+	private HorizontalLayout gitAndOsBtnsLayout = new HorizontalLayout();
+	private HorizontalLayout cnlAndValBtnsLayout = new HorizontalLayout();
+	private HorizontalLayout advancedLayout = new HorizontalLayout();
+	private VerticalLayout envLayout = new VerticalLayout();
 	private VerticalLayout pkgPanelLayout = new VerticalLayout();
 	private VerticalLayout pkgListLayout = new VerticalLayout();
 	private VerticalLayout pkgDescrLayout = new VerticalLayout();
@@ -70,56 +91,39 @@ public class AppsUI extends UI {
 	private AddOSWindow osWindow = new AddOSWindow();
 	private List<Object> pkgItems;
 	private EnvironmentForm env = new EnvironmentForm();
-
-	JSONParser parser = new JSONParser();
-	JSONObject spackPackages;
-	Object obj;
-	
 	private List<Package> selectedPackages = new LinkedList<Package>();
-	
-	
-    @Override
-    protected void init(VaadinRequest vaadinRequest) {
-    	EnvironmentManager factory = AppsFactory.eINSTANCE.createEnvironmentManager();
-    	//IEnvironment environment = factory.createEmpty("Docker");
-    	
-    	
-    	searchTxtField.setWidth("100%");
-    	searchTxtField.setDescription("type filter text");
-    	searchTxtField.setInputPrompt("type filter text");
-    	
-    	String path = VaadinService.getCurrent().getBaseDirectory().getAbsolutePath();
-    	
-		try {
-			obj = parser.parse(new FileReader(
-					path + "/WEB-INF/classes/packages.json"	));
-			spackPackages = (JSONObject) obj;
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (ParseException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-		JSONArray spackPkgs = (JSONArray) spackPackages.get("packages");
-		Iterator<JSONObject> iterator = spackPkgs.iterator();
-		while (iterator.hasNext()) {
+
+	/**
+	 * The OSGi bundle for this running service.
+	 */
+	private static Bundle bundle;
+
+	@Override
+	protected void init(VaadinRequest vaadinRequest) {
+
+		EnvironmentManager factory = AppsFactory.eINSTANCE.createEnvironmentManager();
+		// IEnvironment environment = factory.createEmpty("Docker");
+
+		searchTxtField.setWidth("100%");
+		searchTxtField.setDescription("type filter text");
+		searchTxtField.setInputPrompt("type filter text");
+
+		// This is just a dummy list - it is empty for now. Don't use a data
+		// structure like this, use an EMF class! ~JJB
+		List<Map<String, String>> spackPackages = new ArrayList<Map<String, String>>();
+
+		for (Map<String, String> spackPackage : spackPackages) {
 			final PackageListItem pkg = new PackageListItem();
-			List lst = new LinkedList();
-			JSONObject jsonPkg = iterator.next();
-			pkg.getPkgChBox().setCaption((String) jsonPkg.get("name"));
-			pkg.getPkgDescrTxtField().setCaption((String) jsonPkg.get("description"));
-			pkg.getPkgChBox().addValueChangeListener( e -> {
+			List lst = new LinkedList(); // Why is this untyped? ~JJB 20170314
+			pkg.getPkgChBox().setCaption((String) spackPackage.get("name"));
+			pkg.getPkgDescrTxtField().setCaption((String) spackPackage.get("description"));
+			pkg.getPkgChBox().addValueChangeListener(e -> {
 				Label pkgDescItem = new Label(pkg.getPkgChBox().getCaption());
 				Label temp = null;
 				if (pkg.getPkgChBox().getValue().equals(true)) {
-					//SpackPackageImpl spackPackageImpl = new SpackPackageImpl();
-					
-					
+					// SpackPackageImpl spackPackageImpl = new
+					// SpackPackageImpl();
+
 					lst.add(pkg.getPkgChBox().getCaption());
 					pkgDescrLayout.addComponent(pkgDescItem);
 				} else if (lst.contains(pkg.getPkgChBox().getCaption())) {
@@ -137,81 +141,101 @@ public class AppsUI extends UI {
 			pkgPanelLayout.addComponent(pkg);
 
 		}
-    	pkgsListPanel.setContent(pkgPanelLayout);
-    	pkgsListPanel.setHeight("100%");
-    	pkgsListPanel.setWidth("100%");
-    	
-    	pkgsListPanel.setResponsive(true);
-    	
-    	pkgsDescrPanel.setWidth("100%");
-    	pkgsDescrPanel.setHeight("100%");
-    	pkgsDescrPanel.setContent(pkgDescrLayout);
-    	
-    	pkgListLayout.addComponents(searchTxtField, pkgsListPanel);
-    	pkgListLayout.setHeight("100%");
-    	pkgListLayout.setExpandRatio(pkgsListPanel, 1.0f);
-    	
-    	packagesLayout.addComponents(pkgListLayout, pkgsDescrPanel);
-    	packagesLayout.setSpacing(true);
-    	packagesLayout.setMargin(new MarginInfo(true,true,false,true));
-    	packagesLayout.setSizeFull();
-    	packagesLayout.setHeight("450px");
-    	mainLayout.addComponent(packagesLayout);
-    	
-    	addRepoButton.addClickListener( e -> {
-    		repoWindow.setHeight("350");
-    		repoWindow.setWidth("460");
-    		addWindow(repoWindow);
-    	});
-    	addOSButton.addClickListener( e -> {
-    		osWindow.setHeight("400");
-    		osWindow.setWidth("500");
-    		addWindow(osWindow);
-    	});
-    	
-    	
-    	gitAndOsBtnsLayout.addComponents(addRepoButton, addOSButton);
-    	gitAndOsBtnsLayout.setSpacing(true);
-    	gitAndOsBtnsLayout.setMargin(new MarginInfo(false,true,false,true));
+		pkgsListPanel.setContent(pkgPanelLayout);
+		pkgsListPanel.setHeight("100%");
+		pkgsListPanel.setWidth("100%");
 
-    	advancedButton.addClickListener( e -> {
-    		if (advancedButton.getIcon().equals(FontAwesome.CARET_RIGHT)) {
-    			advancedButton.setIcon(FontAwesome.CARET_DOWN);
-    			envLayout.addComponent(env);
-    		} else {
-    			advancedButton.setIcon(FontAwesome.CARET_RIGHT);
-    			envLayout.removeComponent(env);
-    		}
-    	});
-    	advancedButton.setWidth("25px");
+		pkgsListPanel.setResponsive(true);
+
+		pkgsDescrPanel.setWidth("100%");
+		pkgsDescrPanel.setHeight("100%");
+		pkgsDescrPanel.setContent(pkgDescrLayout);
+
+		pkgListLayout.addComponents(searchTxtField, pkgsListPanel);
+		pkgListLayout.setHeight("100%");
+		pkgListLayout.setExpandRatio(pkgsListPanel, 1.0f);
+
+		packagesLayout.addComponents(pkgListLayout, pkgsDescrPanel);
+		packagesLayout.setSpacing(true);
+		packagesLayout.setMargin(new MarginInfo(true, true, false, true));
+		packagesLayout.setSizeFull();
+		packagesLayout.setHeight("450px");
+		mainLayout.addComponent(packagesLayout);
+
+		addRepoButton.addClickListener(e -> {
+			repoWindow.setHeight("350");
+			repoWindow.setWidth("460");
+			addWindow(repoWindow);
+		});
+		addOSButton.addClickListener(e -> {
+			osWindow.setHeight("400");
+			osWindow.setWidth("500");
+			addWindow(osWindow);
+		});
+
+		gitAndOsBtnsLayout.addComponents(addRepoButton, addOSButton);
+		gitAndOsBtnsLayout.setSpacing(true);
+		gitAndOsBtnsLayout.setMargin(new MarginInfo(false, true, false, true));
+
+		advancedButton.addClickListener(e -> {
+			if (advancedButton.getIcon().equals(FontAwesome.CARET_RIGHT)) {
+				advancedButton.setIcon(FontAwesome.CARET_DOWN);
+				envLayout.addComponent(env);
+			} else {
+				advancedButton.setIcon(FontAwesome.CARET_RIGHT);
+				envLayout.removeComponent(env);
+			}
+		});
+		advancedButton.setWidth("25px");
 		advancedButton.addStyleName("borderless");
-		
+
 		advancedLayout.addComponents(advancedButton, advancedLabel);
 		advancedLayout.setComponentAlignment(advancedLabel, Alignment.MIDDLE_RIGHT);
 		advancedLayout.setSpacing(true);
-		advancedLayout.setMargin(new MarginInfo(false,true,false,true));
-    	
+		advancedLayout.setMargin(new MarginInfo(false, true, false, true));
+
 		cancelButton.setWidth("130px");
 		validateButton.setWidth("130px");
-		
-		validateButton.addClickListener( e -> {
-			
+
+		validateButton.addClickListener(e -> {
+
 		});
-		
+
 		cnlAndValBtnsLayout.addComponents(cancelButton, validateButton);
-    	cnlAndValBtnsLayout.setMargin(new MarginInfo(false,true,false,false));
+		cnlAndValBtnsLayout.setMargin(new MarginInfo(false, true, false, false));
 
 		cnlAndValBtnsLayout.setSpacing(true);
 		mainLayout.addComponents(gitAndOsBtnsLayout, advancedLayout, envLayout, cnlAndValBtnsLayout);
 		mainLayout.setComponentAlignment(cnlAndValBtnsLayout, Alignment.BOTTOM_RIGHT);
-    	mainLayout.setMargin(true);
-    	mainLayout.setSpacing(true);
-    	
-    	setContent(mainLayout);
-    }
+		mainLayout.setMargin(true);
+		mainLayout.setSpacing(true);
 
-    @WebServlet(urlPatterns = "/*", name = "AppsUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = AppsUI.class, productionMode = false)
-    public static class AppsUIServlet extends VaadinServlet {
-    }
+		setContent(mainLayout);
+	}
+
+	@WebServlet(urlPatterns = "/*", name = "AppsUIServlet", asyncSupported = true)
+	@VaadinServletConfiguration(ui = AppsUI.class, productionMode = false)
+	public static class AppsUIServlet extends VaadinServlet {
+	}
+
+	/**
+	 * OSGi bundle activator with annotation instead of activator class.
+	 * 
+	 * @param context
+	 */
+	@Activate
+	public void start(BundleContext context) {
+		System.out.println("App Store VAADIN bundle started.");
+		bundle = context.getBundle();
+		try {
+			AppsUI.httpService.registerServlet("/", new AppsUIServlet(), null, null);
+		} catch (ServletException | NamespaceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void setService(HttpService httpService) {
+		AppsUI.httpService = httpService;
+	}
 }
