f29dadb16c2f01acd35a7daca99e1e0dfbc463fa
Joe Osborn <osbornjd@ornl.gov>
2019-08-15 13:25:46 -0400

    Some test classes
    Signed-off-by: Joe Osborn <osbornjd@ornl.gov>

---
+++ org.eclipse.ice.commands/src/test/java/org/eclipse/ice/tests/commands/ConnectionConfigurationTest.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2019- UT-Battelle, LLC.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Initial API and implementation and/or initial documentation - 
+ *   Jay Jay Billings, Joe Osborn
+ *******************************************************************************/
+package org.eclipse.ice.tests.commands;
+
+import static org.junit.Assert.fail;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * This class tests {@link org.eclipse.ice.commands.ConnectionConfiguration}.
+ * 
+ * @author Jay Jay Billings
+ *
+ */
+public class ConnectionConfigurationTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * Test method for
+	 * {@link org.eclipse.ice.commands.ConnectionConfiguration#ConnectionConfiguration()}.
+	 */
+	@Test
+	public void testConnectionConfiguration() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ org.eclipse.ice.commands/src/test/java/org/eclipse/ice/tests/commands/ConnectionTest.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2019- UT-Battelle, LLC.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Initial API and implementation and/or initial documentation - 
+ *   Jay Jay Billings, Joe Osborn
+ *******************************************************************************/
+package org.eclipse.ice.tests.commands;
+
+import static org.junit.Assert.fail;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * This class tests {@link org.eclipse.ice.commands.Connection}.
+ * 
+ * @author Jay Jay Billings
+ *
+ */
+public class ConnectionTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link org.eclipse.ice.commands.Connection#Connection()}.
+	 */
+	@Test
+	public void testConnection() {
+		fail("Not yet implemented");
+	}
+
+}
