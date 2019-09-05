0d27acdd40c2b7d77e0c50317a9d6ae4cb688e77
Jay Jay Billings <billingsjj@ornl.gov>
2019-09-05 12:48:21 -0400

    Hooked FileHandler up to the workflow code.
    Signed-off-by: Jay Jay Billings <billingsjj@ornl.gov>

--- org.eclipse.ice.workflow/src/main/java/org/eclipse/ice/workflow/TaskTest.java
+++ org.eclipse.ice.workflow/src/main/java/org/eclipse/ice/workflow/TaskTest.java
@@ -1,13 +1,10 @@
 package org.eclipse.ice.workflow;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 import java.util.EnumSet;
 
-import org.eclipse.remote.core.IRemoteProcess;
+import org.eclipse.ice.commands.FileHandlerFactory;
+import org.eclipse.ice.commands.IFileHandler;
 import org.springframework.statemachine.StateContext;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.action.Action;
@@ -19,23 +16,25 @@ public class TaskTest {
 	static String name = "ICEIII/ice/org.eclipse.ice.workflow/src/main/resources/test1.txt";
 	static String newName = "ICEIII/ice/org.eclipse.ice.workflow/src/main/resources/test2.txt";
 
-	IRemoteProcess remoteJob;
-	
 	static public StateMachine<WorkflowEngine.States, WorkflowEngine.Events> buildMachine()
 			throws Exception {
 		Builder<WorkflowEngine.States, WorkflowEngine.Events> builder = new StateMachineBuilder.Builder<>();
 
-		builder.configureStates().withStates().initial(WorkflowEngine.States.INITIALIZED)
+		builder.configureStates().withStates()
+				.initial(WorkflowEngine.States.INITIALIZED)
 				.states(EnumSet.allOf(WorkflowEngine.States.class));
 
-		builder.configureTransitions().withExternal().source(WorkflowEngine.States.INITIALIZED)
+		builder.configureTransitions().withExternal()
+				.source(WorkflowEngine.States.INITIALIZED)
 				.target(WorkflowEngine.States.EXECUTING)
-				.event(WorkflowEngine.Events.PARAMETERS_RECEIVED).and().withExternal()
-				.source(WorkflowEngine.States.EXECUTING).target(WorkflowEngine.States.FINISHED)
+				.event(WorkflowEngine.Events.PARAMETERS_RECEIVED).and()
+				.withExternal().source(WorkflowEngine.States.EXECUTING)
+				.target(WorkflowEngine.States.FINISHED)
 				.action(new Action<WorkflowEngine.States, WorkflowEngine.Events>() {
 
 					@Override
-					public void execute(StateContext<WorkflowEngine.States, WorkflowEngine.Events> context) {
+					public void execute(
+							StateContext<WorkflowEngine.States, WorkflowEngine.Events> context) {
 						try {
 							moveFile(name, newName);
 						} catch (IOException e) {
@@ -50,14 +49,9 @@ public class TaskTest {
 
 	public static void moveFile(final String filePath, final String newFilePath)
 			throws IOException {
-
-		String home = System.getProperty("user.home");
-		String separator = String.valueOf(File.separatorChar);
-		String homePath = home + separator;
-
-		Path source = (new File(homePath + filePath)).toPath();
-		Path target = (new File(homePath + newFilePath)).toPath();
-		Files.move(source, target, StandardCopyOption.REPLACE_EXISTING);
+		FileHandlerFactory factory = new FileHandlerFactory();
+		IFileHandler fileHandler = factory.getFileHandler();
+		fileHandler.move(filePath, newFilePath);
 		return;
 	}
 
