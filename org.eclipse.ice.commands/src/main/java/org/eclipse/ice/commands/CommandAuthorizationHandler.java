372cadaaa19e13be9e40a87fbf646bace2137ce4
osbornjd <osbornjd@ornl.gov>
2019-10-14 08:09:59 -0400

    Initial commit of password authorization design
    Signed-off-by: osbornjd <osbornjd@ornl.gov>

---
+++ org.eclipse.ice.commands/src/main/java/org/eclipse/ice/commands/CommandAuthorizationHandler.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2019- UT-Battelle, LLC.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Initial API and implementation and/or initial documentation - 
+ *   Jay Jay Billings, Joe Osborn
+ *******************************************************************************/
+package org.eclipse.ice.commands;
+
+/**
+ * This class is a super class for the password authorization and authentication
+ * within the commands package. It lays the foundation for the various password
+ * authentication methods that one may use when executing e.g. remote commands.
+ * 
+ * @author Joe Osborn
+ *
+ */
+public abstract interface CommandAuthorizationHandler {
+
+	
+
+}
---
+++ org.eclipse.ice.commands/src/test/java/org/eclipse/ice/tests/commands/ConsoleCommandAuthorizationHandlerTest.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2019- UT-Battelle, LLC.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Initial API and implementation and/or initial documentation - 
+ *   Jay Jay Billings, Joe Osborn
+ *******************************************************************************/
+package org.eclipse.ice.tests.commands;
+
+/**
+ * This class implements test functinoality for the ConsoleCommandAuthorization
+ * class
+ * 
+ * @author Joe Osborn
+ *
+ */
+public class ConsoleCommandAuthorizationHandlerTest {
+
+	/**
+	 * 
+	 */
+	public ConsoleCommandAuthorizationHandlerTest() {
+	}
+
+}
