2bf3c97df62d714011d69d9aa55c8a10712c6ebd
Robert Smith <smithrw@ornl.gov>
2020-03-17 12:15:59 -0400

    Added copyright statements
    Added the Eclipse copyright statment to all source files.
    
    Signed-off-by: Robert Smith <smithrw@ornl.gov>

--- org.eclipse.ice.renderer/src/org/eclipse/ice/renderer/Launcher.java
+++ org.eclipse.ice.renderer/src/org/eclipse/ice/renderer/Launcher.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2020- UT-Battelle, LLC.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Initial API and implementation and/or initial documentation - 
+ *   Jay Jay Billings
+ *******************************************************************************/
 package org.eclipse.ice.renderer;
 
 import com.google.inject.Guice;
