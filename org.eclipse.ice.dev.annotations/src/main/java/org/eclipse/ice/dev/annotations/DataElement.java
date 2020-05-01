aa19ad86dc617303a1798223f7011d717ef0e8ce
Jay Jay Billings <billingsjj@ornl.gov>
2020-05-01 11:27:10 -0400

    Added annotations to the annotations.
    Always wanted to say that. Set scopes, etc.
    
    Signed-off-by: Jay Jay Billings <billingsjj@ornl.gov>

--- org.eclipse.ice.dev.annotations/src/main/java/org/eclipse/ice/dev/annotations/DataElement.java
+++ org.eclipse.ice.dev.annotations/src/main/java/org/eclipse/ice/dev/annotations/DataElement.java
@@ -1,5 +1,12 @@
 package org.eclipse.ice.dev.annotations;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.RetentionPolicy;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
 public @interface DataElement {
 
 }
--- org.eclipse.ice.dev.annotations/src/main/java/org/eclipse/ice/dev/annotations/DataField.java
+++ org.eclipse.ice.dev.annotations/src/main/java/org/eclipse/ice/dev/annotations/DataField.java
@@ -1,7 +1,13 @@
 package org.eclipse.ice.dev.annotations;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
 @Repeatable(DataFields.class)
 public @interface DataField {
 	String fieldName();
