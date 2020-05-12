79729ffe6237aaca294971134a047ffe3972bfd6
Daniel Bluhm <bluhmdj@ornl.gov>
2020-05-12 14:52:47 -0400

    Add properties from DataElement<T>
    Signed-off-by: Daniel Bluhm <bluhmdj@ornl.gov>

--- org.eclipse.ice.renderer/src/main/java/org/eclipse/ice/renderer/Person.java
+++ org.eclipse.ice.renderer/src/main/java/org/eclipse/ice/renderer/Person.java
@@ -5,7 +5,8 @@ import org.eclipse.ice.dev.annotations.DataField;
 
 @DataElement
 @DataField(fieldName = "age", fieldType = String.class)
-@DataField(fieldName = "name", fieldType = String.class)
+@DataField(fieldName = "firstName", fieldType = String.class)
+@DataField(fieldName = "lastName", fieldType = String.class)
 public interface Person {
 	//void foo();
 }
