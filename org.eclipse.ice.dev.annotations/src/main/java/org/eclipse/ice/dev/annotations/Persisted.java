176e1d0706ef00ff147d5a169c1c4396673eb86d
Daniel Bluhm <bluhmdj@ornl.gov>
2020-06-18 13:25:07 -0400

    Persisted Annotation docs.
    Signed-off-by: Daniel Bluhm <bluhmdj@ornl.gov>

--- org.eclipse.ice.dev.annotations/src/main/java/org/eclipse/ice/dev/annotations/Persisted.java
+++ org.eclipse.ice.dev.annotations/src/main/java/org/eclipse/ice/dev/annotations/Persisted.java
@@ -6,8 +6,18 @@ import static java.lang.annotation.RetentionPolicy.SOURCE;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+/**
+ * Mark a DataElement for PersistenceHandler generation.
+ *
+ * The generated class will implement the IPersistenceHandler interface.
+ * @author Daniel Bluhm
+ */
 @Retention(SOURCE)
 @Target(TYPE)
 public @interface Persisted {
+	/**
+	 * The name of the collection to which the generated DataElement class will be
+	 * stored.
+	 */
 	String collection();
 }
