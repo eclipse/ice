faa0ed071a215ae3ad01ac483e03eebcb3e6017f
Daniel Bluhm <bluhmdj@ornl.gov>
2020-06-29 14:40:37 -0400

    Rearrange dev.annotations to add proxytest module
    Signed-off-by: Daniel Bluhm <bluhmdj@ornl.gov>

---
+++ org.eclipse.ice.dev/org.eclipse.ice.dev.annotations.proxytest/src/test/java/org/eclipse/ice/dev/annotations/proxytest/GeneratedDataElementSpec.java
@@ -0,0 +1,9 @@
+package org.eclipse.ice.dev.annotations.proxytest;
+
+import org.eclipse.ice.dev.annotations.DataElement;
+import org.eclipse.ice.dev.annotations.DataField;
+
+@DataElement(name = "GeneratedDataElement")
+public class GeneratedDataElementSpec {
+	@DataField public String testField;
+}
---
+++ org.eclipse.ice.dev/org.eclipse.ice.dev.annotations.proxytest/src/test/java/org/eclipse/ice/dev/annotations/proxytest/JavascriptValidatorTest.java
@@ -0,0 +1,79 @@
+package org.eclipse.ice.dev.annotations.proxytest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.eclipse.ice.dev.annotations.JavascriptValidator;
+import org.junit.jupiter.api.Test;
+
+/**
+ * This class is responsible for testing the ability of the package to do
+ * Javascript-based validation of data classes.
+ * 
+ * @author Jay Jay Billings
+ *
+ */
+class JavascriptValidatorTest {
+
+	/**
+	 * This function tests that Javascript functions can be correctly set on the
+	 * validator and that validation can be run successfully. Since functions are
+	 * simply strings, the first part of the test is a rudimentary accessor test
+	 * only.
+	 */
+	@Test
+	void testValidation() {
+		// Check function accessors
+		JavascriptValidator<String> validator = new JavascriptValidator<String>();
+		String jsFunction = "var checkData = function (data) {return data == 'Solar Fields';}";
+		validator.setFunction(jsFunction);
+		assertEquals(validator.getFunction(), jsFunction);
+		
+		// Check actual validation, which requires executing the Javascript
+		try {
+			assertTrue(validator.validate("Solar Fields"));
+		} catch (NoSuchMethodException e) {
+			// Complain
+			e.printStackTrace();
+			fail();
+		}
+		
+		return;
+	}
+
+	/**
+	 * 
+	 */
+	@Test
+	void testEquality() {
+		JavascriptValidator<String> validator = new JavascriptValidator<String>();
+		String jsFunction = "The Glitch Mob";
+		validator.setFunction(jsFunction);
+		
+		JavascriptValidator<String> validator2 = new JavascriptValidator<String>();
+		validator2.setFunction(jsFunction);
+		
+		// Check basic equality types
+		assertEquals(validator,validator);
+		assertEquals(validator,validator2);
+		JavascriptValidator<String> validator3 = new JavascriptValidator<String>();
+		validator3.setFunction(validator2.getFunction());
+		assertEquals(validator,validator3);
+		// Check some wrong answers
+		validator3.setFunction("Blackmill");
+		assertFalse(validator3.equals(validator));
+		assertFalse(validator.equals(null));
+		
+		// Check cloning
+		assertEquals(validator,validator.clone());
+		
+		// Check copying
+		validator2 = new JavascriptValidator<String>(validator);
+		assertEquals(validator,validator2);
+		
+		// Check the hash codes
+		assertEquals(validator.hashCode(), validator2.hashCode());
+
+		return;
+	}
+
+}
