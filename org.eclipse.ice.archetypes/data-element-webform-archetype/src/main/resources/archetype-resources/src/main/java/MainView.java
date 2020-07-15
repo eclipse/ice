4bd76488eeb8fd967cca61ef561ba2c44d44da6a
Gregory Cage <cagege@ornl.gov>
2020-07-15 10:29:01 -0400

    Add project archetypes for data renderer projects
    Signed-off-by: Gregory Cage <cagege@ornl.gov>

---
+++ org.eclipse.ice.archetypes/data-element-webform-archetype/src/main/resources/archetype-resources/src/main/java/MainView.java
@@ -0,0 +1,21 @@
+package $package;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.router.Route;
+
+/**
+ * The main view contains a button and a click listener.
+ */
+@Route
+@PWA(name = "My Application", shortName = "My Application")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        Button button = new Button("Click me",
+                event -> Notification.show("Clicked!"));
+        add(button);
+    }
+}
---
+++ org.eclipse.ice.archetypes/src/main/java/org/eclipse/ice/App.java
@@ -0,0 +1,13 @@
+package org.eclipse.ice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
