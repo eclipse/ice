2bfc7db4b9cd61b991176840cfaad62536027212
Daniel Bluhm <bluhmdj@ornl.gov>
2020-10-23 15:08:47 -0400

    Revert "Revert changes in deserialization, typescript"
    This reverts commit 50f10a9803bd3b3b756142a3f8c6ac50d28e9e9b.

--- org.eclipse.ice.dev/org.eclipse.ice.dev.annotations/src/main/java/org/eclipse/ice/dev/annotations/processors/DataElementWriterGenerator.java
+++ org.eclipse.ice.dev/org.eclipse.ice.dev.annotations/src/main/java/org/eclipse/ice/dev/annotations/processors/DataElementWriterGenerator.java
@@ -56,6 +56,15 @@ public class DataElementWriterGenerator implements WriterGenerator {
 			.fields(data.getFields())
 			.types(data.getFields().getTypes())
 			.build());
+		try {
+			writers.add(TypeScriptWriter.builder()
+				.name(data.getName())
+				.fields(nonDefaults)
+				.types(nonDefaults.getTypes())
+				.build());
+		} catch (UnsupportedOperationException e) {
+			logger.info("Failed to create typescript writer for element:", e);
+		}
 
 		return writers;
 	}
---
+++ org.eclipse.ice.dev/org.eclipse.ice.dev.annotations/src/main/java/org/eclipse/ice/dev/annotations/processors/TypeScriptWriter.java
@@ -0,0 +1,112 @@
+/*******************************************************************************
+ * Copyright (c) 2020- UT-Battelle, LLC.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Daniel Bluhm - Initial implementation
+ *******************************************************************************/
+
+package org.eclipse.ice.dev.annotations.processors;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Map;
+
+import javax.annotation.processing.Filer;
+import javax.tools.StandardLocation;
+
+import lombok.Builder;
+import lombok.NonNull;
+
+/**
+ * Writer for TypeScript representation of DataElement.
+ * @author Daniel Bluhm
+ */
+public class TypeScriptWriter
+	extends VelocitySourceWriter
+	implements GeneratedFileWriter
+{
+
+	/**
+	 * Template used for this writer.
+	 */
+	private static final String TYPESCRIPT_TEMPLATE = "templates/TypeScript.vm";
+
+	/**
+	 * Context key for name.
+	 */
+	private static final String NAME = "name";
+
+	/**
+	 * Context key for fields.
+	 */
+	private static final String FIELDS = "fields";
+
+	/**
+	 * Context key for types.
+	 */
+	private static final String TYPES = "types";
+
+	/**
+	 * Context key for primitiveMap.
+	 */
+	private static final String PRIMITIVE_MAP = "primitiveMap";
+
+	/**
+	 * Map of Java primitive + String type strings to TypeScript type strings.
+	 */
+	private static Map<String, String> primitiveMap = Map.ofEntries(
+		Map.entry("java.lang.String", "string"),
+		Map.entry("boolean", "boolean"),
+		Map.entry("float", "number"),
+		Map.entry("long", "number"),
+		Map.entry("int", "number"),
+		Map.entry("double", "number"),
+		Map.entry("java.util.Date", "Date")
+	);
+
+	/**
+	 * Name of file generated.
+	 */
+	private String filename;
+
+	/**
+	 * Create Writer.
+	 * @param name of TypeScript class generated.
+	 * @param fields present on data element.
+	 * @param types of fields.
+	 * @throws UnsupportedOperationException When any field is not supported.
+	 */
+	@Builder
+	public TypeScriptWriter(
+		String name, @NonNull Fields fields, @NonNull Types types
+	) {
+		super();
+		for (Field field : fields) {
+			if (!primitiveMap.containsKey(field.getType())) {
+				throw new UnsupportedOperationException(String.format(
+					"Field %s: type %s is unsupported",
+					field.getName(), field.getType()
+				));
+			}
+		}
+		this.template = TYPESCRIPT_TEMPLATE;
+		this.filename = name;
+		this.context.put(NAME, name);
+		this.context.put(FIELDS, fields);
+		this.context.put(TYPES, types);
+		this.context.put(PRIMITIVE_MAP, primitiveMap);
+	}
+
+	@Override
+	public Writer openWriter(Filer filer) throws IOException {
+		return filer.createResource(
+			StandardLocation.SOURCE_OUTPUT,
+			"",
+			String.format("frontend/%s.ts", filename)
+		).openWriter();
+	}
+}
